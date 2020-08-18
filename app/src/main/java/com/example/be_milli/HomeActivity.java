b360a89aa138cdb87a9485974b8d25477326a7d2
muksit77 <muksit77@gmail.com>
2020-08-18 17:55:33 +0600

    LoginActivity & WelcomeActivity created.

--- app/src/main/java/com/example/be_milli/HomeActivity.java
+++ app/src/main/java/com/example/be_milli/HomeActivity.java
@@ -99,7 +99,7 @@ public class  HomeActivity extends AppCompatActivity {
         });
     }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         Button getStarted=findViewById(R.id.getStartedBtn);
