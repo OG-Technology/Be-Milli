182fb6b0418fee2193afd95ad7c22b04f5b40c9e
Sagor <sahadatsagor@sju.ac.kr>
2020-08-19 02:53:41 +0600

    OTP added

--- app/src/main/java/com/example/be_milli/LoginActivity.java
+++ app/src/main/java/com/example/be_milli/LoginActivity.java
@@ -2,13 +2,27 @@ package com.example.be_milli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        number= findViewById(R.id.buttonSignUp);
+
+        number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, NumberActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
