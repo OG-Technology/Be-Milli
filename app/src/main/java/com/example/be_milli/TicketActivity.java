c7503f581404b8198ba34d92d878d0126d30430c
Sagor <sahadatsagor@sju.ac.kr>
2020-10-03 04:46:04 +0600

    add hashmap on database

--- app/src/main/java/com/example/be_milli/TicketActivity.java
+++ app/src/main/java/com/example/be_milli/TicketActivity.java
@@ -1,8 +1,5 @@
 package com.example.be_milli;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -15,13 +12,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.baoyz.swipemenulistview.SwipeMenu;
 import com.baoyz.swipemenulistview.SwipeMenuCreator;
 import com.baoyz.swipemenulistview.SwipeMenuItem;
 import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.example.be_milli.R;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,16 +31,16 @@ import com.google.firebase.database.ValueEventListener;
 import com.robinhood.ticker.TickerUtils;
 import com.robinhood.ticker.TickerView;
 
-import java.awt.font.TextAttribute;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 
 public class TicketActivity extends AppCompatActivity {
     TextView ticketNumberText;
     Button btnTicketGen,btnTicketCon, buttonTicketPay;
     ElegantNumberButton btnTicketNum;
-
+    TickerView tickerView;
 
     FirebaseAuth tAuth;
     FirebaseDatabase TicketDatabase;
@@ -48,79 +48,54 @@ public class TicketActivity extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket);
-        final TickerView tickerView = findViewById(R.id.textTicket);
+        tickerView = findViewById(R.id.textTicket);
         tickerView.setCharacterLists(TickerUtils.provideNumberList());
 
         tAuth=FirebaseAuth.getInstance();
         TicketDatabase=FirebaseDatabase.getInstance();
         TicketReference=TicketDatabase.getReference("Ticket");
         final String userId=tAuth.getCurrentUser().getUid();
-
+        final Map tickets=new HashMap();
 
         btnTicketGen = findViewById(R.id.buttonTicketGen);
         btnTicketCon=findViewById(R.id.buttonTicketCon);
-        btnTicketNum = (ElegantNumberButton) findViewById(R.id.btnTicketNum);
+        buttonTicketPay=findViewById(R.id.buttonTicketPay);
 
         tickerView.setAnimationDuration(3000);
         tickerView.setAnimationInterpolator(new OvershootInterpolator());
         tickerView.setGravity(Gravity.START);
         tickerView.setText("0000000");
 
-        final SwipeMenuListView listView = (SwipeMenuListView) findViewById(R.id.listView);
+        //final SwipeMenuListView listView = (SwipeMenuListView) findViewById(R.id.listView);
         final ArrayList<String> list = new ArrayList<>();
 
 
         final ArrayAdapter adapter = new ArrayAdapter(TicketActivity.this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
+        //listView.setAdapter(adapter);
 
 
 
 
-        btnTicketNum.setOnClickListener(new ElegantNumberButton.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String num = btnTicketNum.getNumber();
-                Log.e("Num", num);
-            }
-        });
+
 
 
         btnTicketGen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Random rn = new Random();
-                //int ticketNumber=rn.nextInt((9999999-1000000)+1)+1000000;
-                int ticketNumber=rn.nextInt(10);
-                final String numberString = String.valueOf(ticketNumber);
 
-                System.out.println("numberString"+numberString);
-                final Query ticketQuery = FirebaseDatabase.getInstance().getReference().child("TicketNumber").orderByChild("ticketNumber").equalTo(numberString);
-                ticketQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ranGen();
 
+            }
+        });
+        buttonTicketPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                        System.out.println("Sagor " + snapshot.getChildrenCount());
-                        if (snapshot.getChildrenCount() > 0) {
-                            Toast.makeText(TicketActivity.this, "Same Number", Toast.LENGTH_SHORT).show();
-
-                        } else {
-                            tickerView.setText(numberString);
-
-                        }
-                    }
-
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-
-                });
             }
         });
 
@@ -128,13 +103,19 @@ public class TicketActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //String ticketConfirm=tickerView.getText();
-                //HelperClass helperClass=new HelperClass(ticketConfirm);
+                String ticketConfirm=tickerView.getText();
 
-                System.out.println(tickerView.getText());
-                list.add(tickerView.getText());
-                adapter.notifyDataSetChanged();
-                TicketReference.child(userId).child("ticket").setValue(list);
+                DatabaseReference ticketKey=TicketReference.child("Ticket").child(userId).push();
+                String ticketPushId=ticketKey.getKey();
+
+                tickets.put(ticketPushId +"/"+"ticket",ticketConfirm);
+
+                // System.out.println(tickerView.getText());
+                //list.add(tickerView.getText());
+                //adapter.notifyDataSetChanged();
+                TicketReference.updateChildren(tickets);
+                btnTicketCon.setVisibility(View.INVISIBLE);
+                btnTicketGen.setVisibility(View.VISIBLE);
             }
         });
 
@@ -174,7 +155,7 @@ public class TicketActivity extends AppCompatActivity {
             }
         };
 
-        listView.setMenuCreator(creator);
+        /*listView.setMenuCreator(creator);
 
         listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
             @Override
@@ -189,7 +170,40 @@ public class TicketActivity extends AppCompatActivity {
                 // false : close the menu; true : not close the menu
                 return false;
             }
-        });
+        });*/
 
+    }
+    private void ranGen(){
+        Random rn = new Random();
+        //int ticketNumber=rn.nextInt((9999999-1000000)+1)+1000000;
+        int ticketNumber=rn.nextInt(10);
+        final String numberString = String.valueOf(ticketNumber);
+
+        System.out.println("numberString"+numberString);
+        final Query ticketQuery = FirebaseDatabase.getInstance().getReference().child("Ticket").orderByChild("ticket").equalTo(numberString);
+        ticketQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                System.out.println("Sagor " + snapshot.getChildrenCount());
+                if (snapshot.getChildrenCount() > 0) {
+                    //Toast.makeText(TicketActivity.this, "Same Number", Toast.LENGTH_SHORT).show();
+                    ranGen();
+                } else {
+                    tickerView.setText(numberString);
+                    btnTicketCon.setVisibility(View.VISIBLE);
+                    btnTicketGen.setVisibility(View.INVISIBLE);
+
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
     }
 }
